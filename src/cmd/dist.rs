31ef2d41f7d1a6a5332d4be3ab84e3f41077f3e5
MiguelLaura <miguellaura240@gmail.com>
2023-01-26 15:33:30 +0100

    draft 2

--- src/cmd/dist.rs
+++ src/cmd/dist.rs
@@ -1,11 +1,12 @@
 use std::cmp;
+use std::collections::BinaryHeap;
 
 use csv;
 use colored::Colorize;
 
 use CliResult;
 use config::{Config, Delimiter};
-use select::{SelectColumns, Selection};
+use select::{SelectColumns};
 use util;
 
 static USAGE: &'static str = "
@@ -19,13 +20,13 @@ dist options:
                            [default: 10]
     --min <arg>            The minimum from which we start to display
                            the distribution. When not set, will take the
-                           minimum from the csv file.
-                           [default: 0]
+                           minimum from the csv file. If not set, will
+                           search the min in the file.
     --max <arg>            The maximum from which we start to display
                            the distribution. When not set, will take the
-                           maximum from the csv file.
-                           [default: 10]
-    --no-nulls             Don't include NULLs in the dist table.
+                           maximum from the csv file. If not set, will
+                           search the max in the file.
+    --no-nans              Don't include NULLs and letters in the dist table.
     --screen-size <arg>    The size used to output the histogram. Set to '0',
                            it will use the shell size.
                            [default: 0]
@@ -35,6 +36,9 @@ dist options:
                            possible size for a bar will be the sum of the cardinalities
                            of the bars.
                            [default: total]
+    --precision <arg>      The number of digit to keep after the comma. Has to be less
+                           than 20.
+                           [default: 2]
 
 Common options:
     -h, --help             Display this message
@@ -56,11 +60,12 @@ struct Args {
     arg_column: SelectColumns,
     arg_input: Option<String>,
     flag_bins: u64,
-    flag_max: u64, // a passer en option
-    flag_min: u64, // a passer en option
-    flag_no_nulls: bool,
+    flag_max: Option<f64>,
+    flag_min: Option<f64>,
+    flag_no_nans: bool,
     flag_screen_size: usize,
     flag_bar_max: String,
+    flag_precision: u8,
     flag_no_headers: bool,
     flag_delimiter: Option<Delimiter>,
 }
@@ -74,6 +79,18 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         return fail!(format!("Unknown \"{}\" bar-max found", bar_max));
     }
 
+    if let Some(min) = args.flag_min {
+        if let Some(max) = args.flag_max {
+            if max < min {
+                return fail!("min must be less than max");
+            }
+        }
+    }
+
+    if args.flag_precision >= 20 {
+        return fail!("precision must be less than 20");
+    }
+
     let mut bar = Bar {
         screen_size: args.flag_screen_size,
         lines_total: 0,
@@ -84,31 +101,89 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         longest_bar: 0,
     };
 
-    let mut bins: Vec<(String, u64, u64, u64)> = match args.bins_construction() {
-        Ok(bins) => {bins},
-        Err(e) => return fail!(e),
+    let mut min = match args.flag_min {
+        None => f64::MAX,
+        Some(min) => min,
     };
+    let mut max = match args.flag_max {
+        None => f64::MIN,
+        Some(max) => max,
+    };
+
+    let mut values: Vec<(f64, u64)> = Vec::new();   // prend trop d'espace mémoire, faire un Counter
+    let mut nans = 0;
+    let mut lines_total = 0;
 
     let mut rdr = rconfig.reader()?;
     let headers = rdr.byte_headers()?;
     let sel = rconfig.selection(headers)?;
+    let column_index = *sel.iter().next().unwrap();
+    let mut record = csv::StringRecord::new();
 
-    let (bins_returned, lines_total) = match args.ftable(&sel, rdr.byte_records(), bins.as_mut_slice()) {
-        Ok((bins_returned, lines_total)) => { (bins_returned, lines_total) },
-        Err(e) => return fail!(e),
-    };
-    let bins_clone = bins_returned.clone();
+    while rdr.read_record(&mut record)? {
+        lines_total += 1;
+        let cell = record[column_index].to_owned();
+        let value = match cell.parse::<f64>() {
+            Ok(nb) => nb,
+            Err(_) => {
+                if !args.flag_no_nans {
+                    nans += 1;
+                }
+                continue
+            }
+        };
+        values.push((value, 1));
+        if args.flag_min.is_none() && value < min as f64 {
+            min = value;
+        }
+        if args.flag_max.is_none() && value > max as f64 {
+            max = value;
+        }
+    }
+    min = floor_float(min, args.flag_precision);
+    max = ceil_float(max, args.flag_precision);
+    if min > max {
+        if values.len() == 0 {
+            return fail!("No result because the colum is empty");
+        }
+        return fail!("No result because min is greater than max");
+    }
+    let max_nb_str_len = cmp::max(
+        format_number_float(min, args.flag_precision).chars().count(),
+        format_number_float(max, args.flag_precision).chars().count()
+    );
+    let max_label_len = cmp::max(max_nb_str_len * 2 + 3, 4);
 
     bar.lines_total = lines_total;
-    match bar.update_sizes(args.flag_max) {
+    match bar.update_sizes(max_label_len) {
         Ok(_) => {},
         Err(e) => return fail!(e),
     };
     bar.print_title();
 
+    let mut bins: Vec<(f64, f64, u64)> = match args.bins_construction(min, max, args.flag_precision) {
+        Ok(bins) => { bins },
+        Err(e) => return fail!(e),
+    };
+    let bins_len = bins.clone().len();
+
+    for (value, count) in values {      // DOUBLE BOUCLE qui peut être évitée ? par exemple, value déjà trié, donc on avance en parallèle -> value est un tas ??
+        for (i, (min, max, _)) in bins.clone().into_iter().enumerate() {
+            if max > value && value >= min {
+                bins[i].2 += count;
+                break;
+            }
+            if value == max {
+                bins[i].2 += count;
+                break;
+            }
+        }
+    }
+    bins[bins_len - 1].2 += nans;
+
     bar.longest_bar = 0;
     if bar_max == "max" {
-        for (_, _, _, count) in bins_returned.into_iter() {
+        for (_, _, count) in bins.clone().into_iter() {
             if count as usize > bar.longest_bar {
                 bar.longest_bar = count as usize;
             }
@@ -118,9 +193,17 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     }
 
     let mut lines_done = 0;
-    for (j, res) in bins_clone.into_iter().enumerate() {
-        lines_done += res.3;
-        bar.print_bar(res.0, res.3 as u64, j);
+    for (j, res) in bins.into_iter().enumerate() {
+        lines_done += res.2;
+        if res.0 == 0.0 && res.1 == 0.0 {
+            let interval = "NaNs";
+            bar.print_bar(interval.to_string(), res.2 as u64, j);
+        } else {
+            let min_interval = format_number_float(res.0, args.flag_precision);
+            let max_interval = format_number_float(res.1, args.flag_precision);
+            let interval = min_interval + " - " + &" ".repeat(max_nb_str_len - max_interval.chars().count()) + &max_interval;
+            bar.print_bar(interval, res.2 as u64, j);
+        }
     }
 
     let resume =
@@ -135,8 +218,6 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     Ok(())
 }
 
-type ByteString = Vec<u8>;
-
 impl Args {
     fn rconfig(&self) -> Config {
         Config::new(&self.arg_input)
@@ -145,57 +226,29 @@ impl Args {
             .select(self.arg_column.clone())
     }
 
-    fn bins_construction(&self) -> CliResult<Vec<(String, u64, u64, u64)>> {
-        let mut bins: Vec<(String, u64, u64, u64)> = Vec::new();
-        let size_interval = (self.flag_max - self.flag_min) / self.flag_bins;
-        let mut temp_min = self.flag_min;
-        let mut temp_max = temp_min + size_interval;
-        let mut temp_text = format_number(self.flag_min) + "-" + &format_number(temp_max);
-        bins.push((temp_text, temp_min, temp_max, 0));
+    fn bins_construction(&self, min: f64, max: f64, precision: u8) -> CliResult<Vec<(f64, f64, u64)>> {
+        let mut bins: Vec<(f64, f64, u64)> = Vec::new();    // existe une mailleure structure pour améliorer l'insertion des valeurs ??
+        let size_interval = ceil_float(((max - min) / self.flag_bins as f64).abs(), precision);
+        let mut temp_min = min;
+        let mut temp_max = ceil_float(temp_min + size_interval, precision);
+        bins.push((temp_min, temp_max, 0));
         for _ in 1..self.flag_bins {
+            if size_interval == 0.0 {
+                break;
+            }
             temp_min = temp_max;
-            temp_max = temp_min + size_interval;
-            temp_text = format_number(temp_min) + "-" + &format_number(temp_max);
-            bins.push((temp_text, temp_min, temp_max, 0));
+            if ceil_float(temp_min + size_interval, precision) < max {
+                temp_max = ceil_float(temp_min + size_interval, precision);
+                bins.push((temp_min, temp_max, 0));
+            } else {
+                bins.push((temp_min, max, 0));
+                break;
+            };
         }
-        if !self.flag_no_nulls {
-            bins.push(("(NULL)".to_string(), 0, 0, 0));
+        if !self.flag_no_nans {
+            bins.push((0.0, 0.0, 0));
         }
         Ok(bins)
-    }
-
-    fn ftable<I>(&self, sel: &Selection, it: I, bins: &mut [(String, u64, u64, u64)]) -> CliResult<(Vec<(String, u64, u64, u64)>, u64)>
-            where I: Iterator<Item=csv::Result<csv::ByteRecord>> {
-        let bins_len = bins.len();
-        let mut count = 0;
-        for row in it {
-            let row = row?;
-            for field in sel.select(&row) {
-                count += 1;
-                let field = trim(field.to_vec());
-                if !field.is_empty() {
-                    let field_value = field.parse::<u64>().unwrap();
-                    for (i, (_, min, max, _)) in bins.into_iter().enumerate() {
-                        if *max > field_value && field_value >= *min {
-                            bins[i].3 += 1;
-                            break;
-                        }
-                    }
-                } else {
-                    if !self.flag_no_nulls {
-                        bins[bins_len - 1].3 += 1;
-                    }
-                }
-            }
-        }
-        Ok((bins.to_vec(), count))
-    }
-}
-
-fn trim(bs: ByteString) -> String {
-    match String::from_utf8(bs) {
-        Ok(s) => s.trim().to_string(),
-        Err(bs) => bs.to_string(),
     }
 }
 
@@ -219,6 +272,61 @@ fn format_number(count: u64) -> String {
     return count_str;
 }
 
+fn format_number_float(count: f64, precision: u8) -> String {
+    let neg = count < 0.0;
+    let mut count_str = count.abs().to_string();
+    let count_str_len = count_str.chars().count();
+    let mut count_str_whole_len = count_str_len;
+    if let Some(idx) = count_str.find(".") {
+        count_str_whole_len = idx;
+    }
+    let count_chars: Vec<char> = count_str.chars().collect();
+    count_str = count_chars[0].to_string();
+    for k in 1..count_str_whole_len {
+        if k % 3 == count_str_whole_len % 3 {
+            count_str += ",";
+        }
+        count_str += &count_chars[k].to_string();
+    }
+    if count_str_whole_len == count_str_len {
+        if precision != 0 {
+            count_str += ".";
+            count_str += &"0".repeat(precision as usize);
+        }
+    } else {
+        for k in count_str_whole_len..count_str_len {
+            count_str += &count_chars[k].to_string();
+        }
+        if (count_str_len -  count_str_whole_len) < (precision + 1) as usize {
+            count_str += &"0".repeat((precision + 1) as usize - (count_str_len -  count_str_whole_len));   
+        }
+    }
+    if neg {
+        count_str = "-".to_string() + &count_str;
+    }
+    return count_str;
+}
+
+fn ceil_float(value: f64, precision: u8) -> f64 {
+    let mul =
+        if precision == 1 {
+            1.0
+        } else {
+            u64::pow(10, precision as u32) as f64
+        };
+    return (value * mul).ceil() / mul;
+}
+
+fn floor_float(value: f64, precision: u8) -> f64 {
+    let mul =
+        if precision == 1 {
+            1.0
+        } else {
+            u64::pow(10, precision as u32) as f64
+        };
+    return (value * mul).floor() / mul;
+}
+
 struct Bar {
     screen_size: usize,
     lines_total: u64,
@@ -231,7 +339,7 @@ struct Bar {
 
 impl Bar {
 
-    fn update_sizes(&mut self, flag_max: u64) -> CliResult<()> {
+    fn update_sizes(&mut self, max_str_len: usize) -> CliResult<()> {
         if self.screen_size == 0 {
             if let Some(size) = termsize::get() {
                 self.screen_size = size.cols as usize;
@@ -251,7 +359,7 @@ impl Bar {
         if self.screen_size <= (legend_str_len + 2) {
             return fail!(format!("Too many lines in the input, we are not able to output the distribution."));
         }
-        self.size_labels = cmp::max(cmp::max(self.lines_total_str_len, format_number(flag_max).chars().count()) * 2 + 1, 6);
+        self.size_labels = max_str_len + 1;
         self.size_bar_cols = self.screen_size - (legend_str_len + 1) - (self.size_labels + 1);
         Ok(())
     }
@@ -269,7 +377,6 @@ impl Bar {
 
         let value = " ".repeat(self.size_labels - value.chars().count()) + &value.to_string();
         let count_str = format_number(count);
-
         let mut nb_square = count as usize * self.size_bar_cols / self.longest_bar;
         let mut bar_str = square_chars[8].repeat(nb_square);
         
@@ -278,10 +385,11 @@ impl Bar {
         bar_str += square_chars[remainder % 8];
 
         let colored_bar_str =
-            if j % 2 == 0
-                { bar_str.dimmed().white() }
-            else
-                { bar_str.white() };
+            if j % 2 == 0 {
+                bar_str.dimmed().white()
+            } else {
+                bar_str.white()
+            };
 
         if remainder % 8 != 0 {
             nb_square += 1;
