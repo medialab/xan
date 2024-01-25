75d53c5a3640708ae96eb2280e03627660323345
Yomguithereal <guillaumeplique@gmail.com>
2024-01-25 16:59:06 +0100

    Fixing moonblade::utils::downgrade_float

--- src/moonblade/utils.rs
+++ src/moonblade/utils.rs
@@ -1,9 +1,28 @@
+const LARGE_EPSILON: f64 = f64::EPSILON * 2.0;
+
 pub fn downgrade_float(f: f64) -> Option<i64> {
-    let t = f.trunc();
+    let dust = f.fract();
 
-    if f - t <= f64::EPSILON {
-        return Some(t as i64);
+    if dust + LARGE_EPSILON >= 1.0 || dust - LARGE_EPSILON <= 0.0 {
+        Some(f.round() as i64)
+    } else {
+        None
     }
+}
 
-    None
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_downgrade_float() {
+        assert_eq!(downgrade_float(0.0), Some(0i64));
+        assert_eq!(downgrade_float(0.0), Some(0i64));
+        assert_eq!(downgrade_float(0.5), None);
+        assert_eq!(downgrade_float(f64::EPSILON), Some(0i64));
+        assert_eq!(downgrade_float(-1.0 + f64::EPSILON), Some(-1i64));
+        assert_eq!(downgrade_float(-1.0 - f64::EPSILON), Some(-1i64));
+        assert_eq!(downgrade_float(1.0 + f64::EPSILON), Some(1i64));
+        assert_eq!(downgrade_float(1.0 - f64::EPSILON), Some(1i64));
+    }
 }
