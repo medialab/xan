354de4c0248a9922dde2ff25159d6623c7eb52e3
Yomguithereal <guillaumeplique@gmail.com>
2024-02-09 15:11:04 +0100

    Add missing count & reverse tests

--- tests/test_count.rs
+++ tests/test_count.rs
@@ -13,4 +13,54 @@ fn count() {
     assert_eq!(got.trim(), "2");
 }
 
-// TODO: test --no-headers
+#[test]
+fn count_no_headers() {
+    let wrk = Workdir::new("count_no_headers");
+    wrk.create("data.csv", vec![svec!["1"], svec!["2"]]);
+
+    let mut cmd = wrk.command("count");
+    cmd.arg("data.csv").arg("-n");
+
+    let got: String = wrk.stdout(&mut cmd);
+
+    assert_eq!(got.trim(), "2");
+}
+
+#[test]
+fn count_no_rows() {
+    let wrk = Workdir::new("count_no_rows");
+    wrk.create("data.csv", vec![svec!["n"]]);
+
+    let mut cmd = wrk.command("count");
+    cmd.arg("data.csv");
+
+    let got: String = wrk.stdout(&mut cmd);
+
+    assert_eq!(got.trim(), "0");
+}
+
+#[test]
+fn count_empty() {
+    let wrk = Workdir::new("count_empty");
+    wrk.create::<Vec<Vec<String>>>("data.csv", vec![]);
+
+    let mut cmd = wrk.command("count");
+    cmd.arg("data.csv");
+
+    let got: String = wrk.stdout(&mut cmd);
+
+    assert_eq!(got.trim(), "0");
+}
+
+#[test]
+fn count_empty_no_headers() {
+    let wrk = Workdir::new("count_empty_no_headers");
+    wrk.create::<Vec<Vec<String>>>("data.csv", vec![]);
+
+    let mut cmd = wrk.command("count");
+    cmd.arg("data.csv").arg("-n");
+
+    let got: String = wrk.stdout(&mut cmd);
+
+    assert_eq!(got.trim(), "0");
+}
--- tests/test_reverse.rs
+++ tests/test_reverse.rs
@@ -1,1 +1,54 @@
-// TODO: test reverse once again...
+use workdir::Workdir;
+
+#[test]
+fn reverse() {
+    let wrk = Workdir::new("reverse");
+    wrk.create(
+        "data.csv",
+        vec![svec!["n"], svec!["1"], svec!["2"], svec!["3"]],
+    );
+    let mut cmd = wrk.command("reverse");
+    cmd.arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![svec!["n"], svec!["3"], svec!["2"], svec!["1"]];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn reverse_no_headers() {
+    let wrk = Workdir::new("reverse_no_headers");
+    wrk.create("data.csv", vec![svec!["1"], svec!["2"], svec!["3"]]);
+    let mut cmd = wrk.command("reverse");
+    cmd.arg("-n").arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![svec!["3"], svec!["2"], svec!["1"]];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn reverse_empty() {
+    let wrk = Workdir::new("reverse_empty");
+    wrk.create::<Vec<Vec<String>>>("data.csv", vec![]);
+    let mut cmd = wrk.command("reverse");
+    cmd.arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    assert_eq!(got, Vec::<Vec<String>>::new());
+}
+
+#[test]
+fn reverse_in_memory() {
+    let wrk = Workdir::new("reverse_in_memory");
+    wrk.create(
+        "data.csv",
+        vec![svec!["n"], svec!["1"], svec!["2"], svec!["3"]],
+    );
+    let mut cmd = wrk.command("reverse");
+    cmd.arg("-m").arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![svec!["n"], svec!["3"], svec!["2"], svec!["1"]];
+    assert_eq!(got, expected);
+}
