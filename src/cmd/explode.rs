5ba2810f2eb6feeb59b7ba61e11f7d168e0e3573
Yomguithereal <guillaumeplique@gmail.com>
2024-02-28 17:02:56 +0100

    Possibility to explode on several aligned columns Related to #110

--- src/cmd/explode.rs
+++ src/cmd/explode.rs
@@ -2,14 +2,60 @@ use csv;
 
 use config::{Config, Delimiter};
 use select::SelectColumns;
-use util::{self, ImmutableRecordHelpers};
+use util;
+use CliError;
 use CliResult;
 
-static USAGE: &str = "
-Explodes a row into multiple ones by splitting a column value based on the
-given separator.
+// NOTE: shamelessly stolen from:
+// https://stackoverflow.com/questions/38821671/how-can-slices-be-split-using-another-slice-as-a-delimiter
+struct SplitSubsequence<'a, 'b, T: 'a + 'b> {
+    slice: &'a [T],
+    needle: &'b [T],
+    ended: bool,
+}
 
-This is the reverse of the implode command.
+impl<'a, 'b, T: 'a + 'b + PartialEq> Iterator for SplitSubsequence<'a, 'b, T> {
+    type Item = &'a [T];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.ended {
+            None
+        } else if self.slice.is_empty() {
+            self.ended = true;
+            Some(self.slice)
+        } else if let Some(p) = self
+            .slice
+            .windows(self.needle.len())
+            .position(|w| w == self.needle)
+        {
+            let item = &self.slice[..p];
+            self.slice = &self.slice[p + self.needle.len()..];
+            Some(item)
+        } else {
+            self.ended = true;
+            let item = self.slice;
+            self.slice = &self.slice[self.slice.len() - 1..];
+            Some(item)
+        }
+    }
+}
+
+fn split_subsequence<'a, 'b, T>(slice: &'a [T], needle: &'b [T]) -> SplitSubsequence<'a, 'b, T>
+where
+    T: 'a + 'b + PartialEq,
+{
+    SplitSubsequence {
+        slice,
+        needle,
+        ended: false,
+    }
+}
+
+static USAGE: &str = "
+Explodes a row into multiple ones by splitting column values by using the
+provided separator.
+
+This is the reverse of the 'implode' command.
 
 For instance the following CSV:
 
@@ -17,22 +63,23 @@ name,colors
 John,blue|yellow
 Mary,red
 
-Can be exploded on the \"colors\" <column> based on the \"|\" <separator> to:
+Can be exploded on the \"colors\" column using the \"|\" <separator> to produce:
 
 name,colors
 John,blue
 John,yellow
 Mary,red
 
-Note that given file needs to be UTF-8 encoded if given separator is more than
-one byte long.
+Note finally that the file can be exploded on multiple well-aligned columns.
 
 Usage:
-    xan explode [options] <column> <separator> [<input>]
+    xan explode [options] <columns> <separator> [<input>]
     xan explode --help
 
 explode options:
-    -r, --rename <name>    New name for the exploded column.
+    -r, --rename <name>    New names for the exploded columns. Must be written
+                           in CSV format if exploding multiple columns.
+                           See 'xsv rename' help for more details.
 
 Common options:
     -h, --help             Display this message
@@ -45,7 +92,7 @@ Common options:
 
 #[derive(Deserialize)]
 struct Args {
-    arg_column: SelectColumns,
+    arg_columns: SelectColumns,
     arg_separator: String,
     arg_input: Option<String>,
     flag_rename: Option<String>,
@@ -59,46 +106,87 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     let rconfig = Config::new(&args.arg_input)
         .delimiter(args.flag_delimiter)
         .no_headers(args.flag_no_headers)
-        .select(args.arg_column);
+        .select(args.arg_columns);
 
     let mut rdr = rconfig.reader()?;
     let mut wtr = Config::new(&args.flag_output).writer()?;
 
-    let headers = rdr.byte_headers()?.clone();
-    let column_index = rconfig.single_selection(&headers)?;
+    let mut headers = rdr.byte_headers()?.clone();
+    let sel = rconfig.selection(&headers)?;
 
-    let mut headers = rdr.headers()?.clone();
+    if sel.is_empty() {
+        return Err(CliError::Other(
+            "expecting a non-empty column selection".to_string(),
+        ));
+    }
 
-    if let Some(new_name) = args.flag_rename {
-        headers = headers.replace_at(column_index, &new_name);
+    // NOTE: the mask deduplicates
+    let sel_mask = sel.indexed_mask(headers.len());
+
+    if let Some(new_names) = args.flag_rename {
+        let new_names = util::str_to_csv_byte_record(&new_names);
+
+        if new_names.len() != sel.len() {
+            return fail!(format!(
+                "Renamed columns alignement error. Expected {} names and got {}.",
+                sel.len(),
+                new_names.len(),
+            ));
+        }
+
+        headers = headers
+            .iter()
+            .zip(sel_mask.iter())
+            .map(|(h, rh)| if let Some(i) = rh { &new_names[*i] } else { h })
+            .collect();
     }
 
     if !rconfig.no_headers {
-        wtr.write_record(&headers)?;
+        wtr.write_byte_record(&headers)?;
     }
 
-    let sep = args.arg_separator;
+    let sep = args.arg_separator.as_bytes();
+    let single_byte_sep = if sep.len() == 1 { Some(&sep[0]) } else { None };
 
-    // NOTE: if separator is a single byte, we don't have to decode
-    if sep.as_bytes().len() == 1 {
-        let sep = &sep.as_bytes()[0];
+    let mut record = csv::ByteRecord::new();
 
-        let mut record = csv::ByteRecord::new();
+    while rdr.read_byte_record(&mut record)? {
+        let splits: Vec<Vec<&[u8]>> = sel
+            .select(&record)
+            .map(|cell| {
+                if let Some(s) = single_byte_sep {
+                    cell.split(|b| b == s).collect()
+                } else {
+                    split_subsequence(cell, sep).collect()
+                }
+            })
+            .collect();
 
-        while rdr.read_byte_record(&mut record)? {
-            for val in record[column_index].split(|b| b == sep) {
-                let new_record = record.replace_at(column_index, val);
-                wtr.write_record(&new_record)?;
-            }
+        if splits.iter().skip(1).any(|s| s.len() != splits[0].len()) {
+            return Err(CliError::Other(
+                "inconsistent exploded length accross columns.".to_string(),
+            ));
         }
-    } else {
-        let mut record = csv::StringRecord::new();
 
-        while rdr.read_record(&mut record)? {
-            for val in record[column_index].split(&sep) {
-                let new_record = record.replace_at(column_index, val);
-                wtr.write_record(&new_record)?;
-            }
+        if splits[0].is_empty() {
+            wtr.write_byte_record(&record)?;
+            continue;
+        }
+
+        for i in 0..splits[0].len() {
+            let output_record: csv::ByteRecord = record
+                .iter()
+                .zip(sel_mask.iter())
+                .map(|(cell, mask)| {
+                    if let Some(j) = mask {
+                        splits[*j][i]
+                    } else {
+                        cell
+                    }
+                })
+                .collect();
+
+            wtr.write_byte_record(&output_record)?;
         }
     }
 
--- tests/test_explode.rs
+++ tests/test_explode.rs
@@ -78,3 +78,80 @@ fn explode_no_headers() {
     ];
     assert_eq!(got, expected);
 }
+
+#[test]
+fn explode_multichar_sep() {
+    let wrk = Workdir::new("explode_multichar_sep");
+    wrk.create(
+        "data.csv",
+        vec![
+            svec!["name", "colors"],
+            svec!["John", "blue[x]orange[x]red"],
+            svec!["Jack", "yellow[x]green"],
+        ],
+    );
+    let mut cmd = wrk.command("explode");
+    cmd.arg("colors").arg("[x]").arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["name", "colors"],
+        svec!["John", "blue"],
+        svec!["John", "orange"],
+        svec!["John", "red"],
+        svec!["Jack", "yellow"],
+        svec!["Jack", "green"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn explode_multipe_columns() {
+    let wrk = Workdir::new("explode_multipe_columns");
+    wrk.create(
+        "data.csv",
+        vec![
+            svec!["name", "colors", "letters"],
+            svec!["John", "blue|red", "a|b"],
+            svec!["Jack", "", ""],
+        ],
+    );
+    let mut cmd = wrk.command("explode");
+    cmd.arg("colors,letters").arg("|").arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["name", "colors", "letters"],
+        svec!["John", "blue", "a"],
+        svec!["John", "red", "b"],
+        svec!["Jack", "", ""],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn explode_multipe_columns_rename() {
+    let wrk = Workdir::new("explode_multipe_columns_rename");
+    wrk.create(
+        "data.csv",
+        vec![
+            svec!["name", "colors", "letters"],
+            svec!["John", "blue|red", "a|b"],
+            svec!["Jack", "", ""],
+        ],
+    );
+    let mut cmd = wrk.command("explode");
+    cmd.arg("colors,letters")
+        .arg("|")
+        .args(["-r", "color,letter"])
+        .arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["name", "color", "letter"],
+        svec!["John", "blue", "a"],
+        svec!["John", "red", "b"],
+        svec!["Jack", "", ""],
+    ];
+    assert_eq!(got, expected);
+}
