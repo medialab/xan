b0295805c976d4551ba311d43af7723c197b990e
Yomguithereal <guillaumeplique@gmail.com>
2024-08-05 20:07:28 +0200

    unit tests for vocab doc-token

--- tests/test_vocab.rs
+++ tests/test_vocab.rs
@@ -67,3 +67,39 @@ fn vocab_token() {
     ];
     assert_eq!(got, expected);
 }
+
+#[test]
+fn vocab_doc_token() {
+    let wrk = Workdir::new("vocab_doc_token");
+    wrk.create(
+        "data.csv",
+        vec![
+            svec!["doc", "token"],
+            svec!["1", "cat"],
+            svec!["1", "dog"],
+            svec!["1", "cat"],
+            svec!["2", "cat"],
+            svec!["2", "rabbit"],
+        ],
+    );
+    let mut cmd = wrk.command("vocab");
+    cmd.arg("doc-token").arg("doc").arg("token").arg("data.csv");
+
+    let mut got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    got[1..].sort();
+
+    let expected = vec![
+        svec!["doc", "token", "tf", "tfidf", "bm25"],
+        svec!["1", "cat", "2", "0", "0"],
+        svec!["1", "dog", "1", "0.6931471805599453", "0.64072428455121"],
+        svec!["2", "cat", "1", "0", "0"],
+        svec![
+            "2",
+            "rabbit",
+            "1",
+            "0.6931471805599453",
+            "0.7549127709068711"
+        ],
+    ];
+    assert_eq!(got, expected);
+}
