7783c7605fe55e42027cf3a569b2b2b0a0bb0946
Yomguithereal <guillaumeplique@gmail.com>
2024-10-03 12:09:02 +0200

    Adding --csv flag to count command

--- src/cmd/count.rs
+++ src/cmd/count.rs
@@ -13,8 +13,13 @@ given).
 Usage:
     xan count [options] [<input>]
 
+count options:
+    --csv  Output the result as a single column, single row CSV file with
+           a \"count\" header.
+
 Common options:
     -h, --help             Display this message
+    -o, --output <file>    Write output to <file> instead of stdout.
     -n, --no-headers       When set, the first row will not be included in
                            the count.
     -d, --delimiter <arg>  The field delimiter for reading CSV data.
@@ -24,7 +29,9 @@ Common options:
 #[derive(Deserialize)]
 struct Args {
     arg_input: Option<String>,
+    flag_csv: bool,
     flag_no_headers: bool,
+    flag_output: Option<String>,
     flag_delimiter: Option<Delimiter>,
 }
 
@@ -33,6 +40,8 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     let conf = Config::new(&args.arg_input)
         .delimiter(args.flag_delimiter)
         .no_headers(args.flag_no_headers);
+
+    let wconf = Config::new(&args.flag_output);
 
     let count = match conf.indexed()? {
         Some(idx) => idx.count(),
@@ -46,5 +55,22 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
             count
         }
     };
-    Ok(println!("{}", count))
+
+    if args.flag_csv {
+        let mut writer = wconf.writer()?;
+        let mut record = csv::ByteRecord::new();
+        record.push_field(b"count");
+        writer.write_byte_record(&record)?;
+
+        record.clear();
+        record.push_field(format!("{}", count).as_bytes());
+        writer.write_byte_record(&record)?;
+
+        writer.flush()?;
+    } else {
+        let mut writer = wconf.io_writer()?;
+        writeln!(writer, "{}", count)?;
+    }
+
+    Ok(())
 }
