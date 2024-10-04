684c5d107ca841b463c6408292fee14da175c877
Yomguithereal <guillaumeplique@gmail.com>
2024-10-04 15:57:13 +0200

    Refactoring parallel core

--- src/cmd/parallel.rs
+++ src/cmd/parallel.rs
@@ -1,8 +1,9 @@
+use std::num::NonZeroUsize;
 use std::process::{Command, Stdio};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use indicatif::ProgressBar;
-use rayon::prelude::*;
+use rayon::{prelude::*, ThreadPoolBuilder};
 
 use crate::config::{Config, Delimiter};
 use crate::util;
@@ -36,17 +37,24 @@ impl ParallelProgressBar {
     }
 }
 
+// TODO: cat without preprocessing is basically moot
+// TODO: stding handling, csv stdin handling
+
 static USAGE: &str = "
-Parallel todo...
+Count, filter & aggregate CSV datasets split into multiple
+files, in parallel.
 
 Usage:
-    xan parallel (count|cat) [options] [<inputs>...]
+    xan parallel count [options] [<inputs>...]
+    xan parallel cat [options] [<inputs>...]
     xan parallel --help
 
 parallel options:
     -p, --preprocess <op>  Preprocessing command that will run on every
                            file to process.
     --progress             Display a progress bar for the parallel tasks.
+    -t, --threads <n>      Number of threads to use. Will default to a sensible
+                           number based on the available CPUs.
 
 Common options:
     -h, --help             Display this message
@@ -65,13 +73,46 @@ struct Args {
     cmd_cat: bool,
     flag_preprocess: Option<String>,
     flag_progress: bool,
+    flag_threads: Option<NonZeroUsize>,
     flag_output: Option<String>,
     flag_no_headers: bool,
     flag_delimiter: Option<Delimiter>,
 }
 
+impl Args {
+    fn reader(&self, path: &str) -> CliResult<csv::Reader<Box<dyn std::io::Read + Send>>> {
+        Ok(if let Some(_preprocessing) = &self.flag_preprocess {
+            let config = Config::new(&None)
+                .delimiter(self.flag_delimiter)
+                .no_headers(self.flag_no_headers);
+
+            let child = Command::new("cat")
+                .stdin(Stdio::null())
+                .stdout(Stdio::piped())
+                .arg(path)
+                .spawn()
+                .expect("could not spawn preprocessing");
+
+            config.csv_reader_from_reader(Box::new(child.stdout.expect("cannot read child stdout")))
+        } else {
+            let config = Config::new(&Some(path.to_string()))
+                .delimiter(self.flag_delimiter)
+                .no_headers(self.flag_no_headers);
+
+            config.reader()?
+        })
+    }
+}
+
 pub fn run(argv: &[&str]) -> CliResult<()> {
     let args: Args = util::get_args(USAGE, argv)?;
+
+    if let Some(threads) = args.flag_threads {
+        ThreadPoolBuilder::new()
+            .num_threads(threads.get())
+            .build_global()
+            .expect("could not build thread pool!");
+    }
 
     let total_count = AtomicUsize::new(0);
 
@@ -84,31 +125,8 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
 
     args.arg_inputs
         .par_iter()
-        .try_for_each(|name| -> CliResult<()> {
-            let mut reader: csv::Reader<Box<dyn std::io::Read + Send>> =
-                if let Some(_preprocessing) = &args.flag_preprocess {
-                    let config = Config::new(&None)
-                        .delimiter(args.flag_delimiter)
-                        .no_headers(args.flag_no_headers);
-
-                    let child = Command::new("cat")
-                        .stdin(Stdio::null())
-                        .stdout(Stdio::piped())
-                        .arg(name)
-                        .spawn()
-                        .expect("could not spawn preprocessing");
-
-                    config.csv_reader_from_reader(Box::new(
-                        child.stdout.expect("cannot read child stdout"),
-                    ))
-                } else {
-                    let config = Config::new(&Some(name.to_string()))
-                        .delimiter(args.flag_delimiter)
-                        .no_headers(args.flag_no_headers);
-
-                    config.reader()?
-                };
-
+        .try_for_each(|path| -> CliResult<()> {
+            let mut reader = args.reader(path)?;
             let mut record = csv::ByteRecord::new();
             let mut count: usize = 0;
 
