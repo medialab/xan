3c719384397b25a3a227645ff37ca9a0604c4df1
Yomguithereal <guillaumeplique@gmail.com>
2024-10-11 18:00:13 +0200

    Better completions Related to #318

--- src/completion.rs
+++ src/completion.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+
+use glob::glob;
+
 static COMMANDS: [&str; 52] = [
     "agg",
     "behead",
@@ -75,13 +79,57 @@ fn main() {
                 }
             }
         }
-    } else if word_before == "vocab" {
-        if !to_complete.starts_with('-') {
-            for command in VOCAB_SUBCOMMANDS {
-                if command.starts_with(to_complete) {
-                    println!("{}", command);
+    } else if word_before == "vocab" && !to_complete.starts_with('-') {
+        for command in VOCAB_SUBCOMMANDS {
+            if command.starts_with(to_complete) {
+                println!("{}", command);
+            }
+        }
+    }
+
+    // Completing column selectors
+    // TODO: escape the selector name
+    if word_before == "-s"
+        || word_before == "--select"
+        || word_before == "-g"
+        || word_before == "--groupby"
+        || word_before == "select"
+        || word_before == "transform"
+        || word_before == "explode"
+        || word_before == "implode"
+        || word_before == "groupby"
+        || word_before == "partition"
+        || word_before == "plot"
+    {
+        let mut all_headers = Vec::<String>::new();
+
+        for entry in glob("**/*.csv")
+            .unwrap()
+            .chain(glob("**/*.csv.gz").unwrap())
+            .chain(glob("**/*.tsv").unwrap())
+            .chain(glob("**/*.tsv.gz").unwrap())
+            .take(15)
+        {
+            let path = entry.unwrap();
+            let file = match File::open(path) {
+                Ok(f) => f,
+                _ => continue,
+            };
+            let mut reader = csv::Reader::from_reader(file);
+
+            if let Ok(headers) = reader.headers() {
+                for name in headers {
+                    let name = name.to_string();
+
+                    if name.starts_with(to_complete) && !all_headers.contains(&name) {
+                        all_headers.push(name);
+                    }
                 }
             }
+        }
+
+        for name in all_headers {
+            println!("{}", name);
         }
     }
 }
