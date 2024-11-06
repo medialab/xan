448fdea23026e60566bb8dd6a39c6c0c0ddf8c01
Julien Pontoire <pontoirejulien@gmail.com>
2024-11-06 17:22:54 +0100

    Adding unit tests

--- tests/test_sample.rs
+++ tests/test_sample.rs
@@ -22,6 +22,32 @@ fn sample() {
 }
 
 #[test]
+fn sample_grouped() {
+    let wrk = Workdir::new("sample_grouped");
+    wrk.create(
+        "data.csv",
+        vec![
+            svec!["number", "group"],
+            svec!["1", "group1"],
+            svec!["2", "group1"],
+            svec!["3", "group1"],
+            svec!["4", "group2"],
+        ],
+    );
+    let mut cmd = wrk.command("sample");
+    cmd.arg("2").arg("data.csv").args(["--seed", "123"]).args(["-g", "group"]);
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["number", "group"], 
+        svec!["3", "group1"], 
+        svec!["2", "group1"], 
+        svec!["4", "group2"]
+        ];
+    assert_eq!(got, expected);
+}
+
+#[test]
 fn sample_weighted() {
     let wrk = Workdir::new("sample_weighted");
     wrk.create(
@@ -45,6 +71,36 @@ fn sample_weighted() {
         svec!["number", "weight"],
         svec!["3", "0.5"],
         svec!["4", "0.9"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn sample_weighted_grouped() {
+    let wrk = Workdir::new("sample_weighted_grouped");
+    wrk.create(
+        "data.csv",
+        vec![
+            svec!["number", "weight", "group"],
+            svec!["1", "0.001", "group1"],
+            svec!["2", "0.001", "group1"],
+            svec!["3", "0.5", "group1"],
+            svec!["4", "0.9", "group2"],
+        ],
+    );
+    let mut cmd = wrk.command("sample");
+    cmd.arg("2")
+        .args(["--weight", "weight"])
+        .arg("data.csv")
+        .args(["--seed", "123"])
+        .args(["-g", "group"]);
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["number", "weight", "group"],
+        svec!["1", "0.001", "group1"],
+        svec!["3", "0.5", "group1"],
+        svec!["4", "0.9", "group2"],
     ];
     assert_eq!(got, expected);
 }
