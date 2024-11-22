2e51c40715bc989ad8c4c2c860cf919b664baeef
Julien Pontoire <pontoirejulien@gmail.com>
2024-11-22 11:25:26 +0100

    Optimizing

--- src/cmd/to.rs
+++ src/cmd/to.rs
@@ -11,6 +11,7 @@ use crate::util;
 use crate::CliError;
 use crate::CliResult;
 
+static MAX_SAFE_INTEGER: i64 = 9007199254740991;
 static USAGE: &str = "
 Convert a CSV file to a variety of data formats.
 
@@ -39,14 +40,14 @@ impl Args {
     fn convert_to_json<R: Read, W: Write>(mut rdr: csv::Reader<R>, writer: W) -> CliResult<()> {
         let headers = rdr.headers()?.clone();
         let mut record = csv::StringRecord::new();
+        let mut json_object = serde_json::Map::new();
 
         let mut json_array = Vec::new();
 
         while rdr.read_record(&mut record)? {
-            let mut json_object = serde_json::Map::new();
             for (header, value) in headers.iter().zip(record.iter()) {
                 if let Ok(parsed_value) = value.parse::<i64>() {
-                    if parsed_value.abs() < 9007199254740991 {
+                    if parsed_value.abs() < MAX_SAFE_INTEGER {
                         json_object
                             .insert(header.to_string(), json!(value.parse::<f64>().unwrap()));
                         continue;
@@ -55,7 +56,7 @@ impl Args {
                 json_object.insert(header.to_string(), json!(value));
             }
 
-            json_array.push(Value::Object(json_object));
+            json_array.push(Value::Object(json_object.clone()));
         }
         let _ = serde_json::to_writer_pretty(writer, &json_array);
 
@@ -68,12 +69,12 @@ impl Args {
     ) -> CliResult<()> {
         let mut record = csv::StringRecord::new();
         let headers = rdr.headers()?.clone();
+        let mut json_object = serde_json::Map::new();
 
         while rdr.read_record(&mut record)? {
-            let mut json_object = serde_json::Map::new();
             for (header, value) in headers.iter().zip(record.iter()) {
                 if let Ok(parsed_value) = value.parse::<i64>() {
-                    if parsed_value.abs() < 9007199254740991 {
+                    if parsed_value.abs() < MAX_SAFE_INTEGER {
                         json_object
                             .insert(header.to_string(), json!(value.parse::<f64>().unwrap()));
                         continue;
@@ -87,6 +88,7 @@ impl Args {
                 "{}",
                 serde_json::to_string(&json_object).map_err(|e| CliError::Other(e.to_string()))?
             )?;
+            //maybe adding a clear for json_object for keys with no values ?
         }
 
         Ok(())
