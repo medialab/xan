95b8a83872384270a0c54d079a97f2e5f516d44f
Yomguithereal <guillaumeplique@gmail.com>
2025-03-13 16:13:41 +0100

    Various xan scrape optimization and paper cuts hunting Related to #508

--- src/cmd/scrape.rs
+++ src/cmd/scrape.rs
@@ -3,9 +3,12 @@ use std::io::{self, Read};
 use std::iter;
 use std::path::PathBuf;
 
+use bstr::ByteSlice;
+use colored::Colorize;
 use flate2::read::GzDecoder;
 use lazy_static::lazy_static;
 use pariter::IteratorExt;
+use regex::bytes::Regex;
 use scraper::{Html, Selector};
 
 use crate::config::{Config, Delimiter};
@@ -28,7 +31,7 @@ fn open(input_dir: &str, filename: &str) -> io::Result<Box<dyn Read>> {
     })
 }
 
-fn read_to_string(input_dir: &str, filename: &str) -> io::Result<String> {
+fn read_string(input_dir: &str, filename: &str) -> io::Result<String> {
     let mut string = String::new();
 
     open(input_dir, filename)?.read_to_string(&mut string)?;
@@ -36,27 +39,78 @@ fn read_to_string(input_dir: &str, filename: &str) -> io::Result<String> {
     Ok(string)
 }
 
-fn read_to_html(input_dir: &str, filename: &str) -> io::Result<Html> {
-    read_to_string(input_dir, filename).map(|string| Html::parse_document(&string))
+const PREBUFFER_SIZE: usize = 65536;
+
+fn read_max_bytes(input_dir: &str, filename: &str, max: u64) -> io::Result<Vec<u8>> {
+    let mut buffer = Vec::with_capacity(max as usize);
+
+    open(input_dir, filename)?
+        .take(max)
+        .read_to_end(&mut buffer)?;
+
+    Ok(buffer)
+}
+
+fn read_html(input_dir: &str, filename: &str) -> io::Result<Html> {
+    read_string(input_dir, filename).map(|string| Html::parse_document(&string))
 }
 
 enum ScraperTarget<'a> {
-    HtmlCell(&'a str),
+    HtmlCell(&'a [u8]),
     HtmlFile(&'a str, &'a str),
 }
 
+fn guard_invalid_html_cell(cell: &[u8]) -> CliResult<()> {
+    if !cell.is_empty() && !looks_like_html(cell) {
+        Err(format!(
+            "encountered cell value that does not look like HTML: {}!\nDid you forget to give {}?",
+            std::str::from_utf8(cell).unwrap().green(),
+            "-I/--input-dir".cyan()
+        ))?
+    } else {
+        Ok(())
+    }
+}
+
 impl ScraperTarget<'_> {
-    fn read_to_html(&self) -> CliResult<Html> {
+    fn prebuffer_bytes(&self) -> CliResult<Vec<u8>> {
         match self {
-            Self::HtmlCell(cell) => Ok(Html::parse_document(cell)),
-            Self::HtmlFile(input_dir, filename) => Ok(read_to_html(input_dir, filename)?),
+            Self::HtmlCell(cell) => {
+                guard_invalid_html_cell(cell)?;
+
+                Ok(cell[..PREBUFFER_SIZE.min(cell.len())].to_vec())
+            }
+            Self::HtmlFile(input_dir, filename) => {
+                Ok(read_max_bytes(input_dir, filename, PREBUFFER_SIZE as u64)?)
+            }
+        }
+    }
+
+    fn read_html(&self) -> CliResult<Html> {
+        match self {
+            Self::HtmlCell(cell) => {
+                guard_invalid_html_cell(cell)?;
+
+                Ok(Html::parse_document(
+                    std::str::from_utf8(cell).expect("invalid utf-8"),
+                ))
+            }
+            Self::HtmlFile(input_dir, filename) => Ok(read_html(input_dir, filename)?),
         }
     }
 }
 
 // Scraper abstractions
 lazy_static! {
-    static ref TITLE_SELECTOR: Selector = Selector::parse("title").unwrap();
+    static ref HTML_LIKE_REGEX: Regex =
+        Regex::new(r"^\s*<(?:html|head|body|title|meta|link|span|div|img|ul|ol|[ap!?])").unwrap();
+    static ref TITLE_REGEX: Regex = Regex::new(r"<title>(.*?)</title>").unwrap();
+    static ref URLS_IN_HTML_REGEX: Regex =
+        Regex::new(r#"<a[^>]*\shref=(?:"([^"]*)"|'([^']*)'|([^\s>]*))[^>]*>"#).unwrap();
+}
+
+fn looks_like_html(bytes: &[u8]) -> bool {
+    HTML_LIKE_REGEX.is_match(bytes)
 }
 
 #[derive(Clone)]
@@ -72,7 +126,7 @@ impl CustomScraper {
         record: &csv::ByteRecord,
         target: ScraperTarget,
     ) -> CliResult<Vec<Vec<DynamicValue>>> {
-        let html = target.read_to_html()?;
+        let html = target.read_html()?;
 
         if let Some(selector) = &self.foreach {
             Ok(self
@@ -85,7 +139,6 @@ impl CustomScraper {
     }
 }
 
-// TODO: support for partial read and regex scraping
 #[derive(Clone)]
 enum Scraper {
     Title,
@@ -93,20 +146,23 @@ enum Scraper {
 }
 
 impl Scraper {
-    fn scrape_title(&self, target: ScraperTarget) -> CliResult<Option<DynamicValue>> {
-        let html = target.read_to_html()?;
-
-        Ok(html
-            .select(&TITLE_SELECTOR)
-            .next()
-            .map(|title_node| DynamicValue::from(title_node.text().collect::<String>().trim())))
-    }
-
     fn names(&self) -> Box<dyn Iterator<Item = &str> + '_> {
         match self {
             Self::Title => Box::new(iter::once("title")),
             Self::Custom(scraper) => Box::new(scraper.program.names()),
         }
+    }
+
+    fn scrape_title(&self, target: ScraperTarget) -> CliResult<Option<DynamicValue>> {
+        let bytes = target.prebuffer_bytes()?;
+
+        debug_assert!(bytes.len() <= PREBUFFER_SIZE);
+
+        Ok(TITLE_REGEX.captures(&bytes).map(|caps| {
+            DynamicValue::from(html_escape::decode_html_entities(
+                std::str::from_utf8(&caps[1]).unwrap(),
+            ))
+        }))
     }
 
     fn scrape(
@@ -138,22 +194,24 @@ Usage:
 
 scrape options:
     -e, --evaluate <expr>    If given, evaluate the given scraping expression.
-    -f, --foreach <css>      If given, will return one row per element matching
-                             the CSS selector in target document, instead of returning
-                             a single row per document.
     -I, --input-dir <path>   If given, target column will be understood
                              as relative path to read from this input
                              directory instead.
     -k, --keep <column>      Selection of columns from the input to keep in
                              the output.
-    --sep <char>             Separator to use when serializing lists.
-                             [default: |]
     -p, --parallel           Whether to use parallelization to speed up computations.
                              Will automatically select a suitable number of threads to use
                              based on your number of cores. Use -t, --threads if you want to
                              indicate the number of threads yourself.
     -t, --threads <threads>  Parellize computations using this many threads. Use -p, --parallel
                              if you want the number of threads to be automatically chosen instead.
+
+scrape -e/--evaluate options:
+    -f, --foreach <css>  If given, will return one row per element matching
+                         the CSS selector in target document, instead of returning
+                         a single row per document.
+    --sep <char>            Separator to use when serializing lists.
+                         [default: |]
 
 Common options:
     -h, --help             Display this message
@@ -235,6 +293,8 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
 
     let mut writer = Config::new(&args.flag_output).writer()?;
 
+    let padding = scraper.names().count();
+
     if !args.flag_no_headers {
         let mut output_headers = headers.clone();
 
@@ -264,10 +324,17 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
                 move |(index, result)| -> CliResult<(csv::ByteRecord, Vec<Vec<DynamicValue>>)> {
                     let record = result?;
 
-                    let cell = std::str::from_utf8(&record[column_index]).expect("invalid utf-8");
+                    let cell = &record[column_index];
+
+                    if cell.trim().is_empty() {
+                        return Ok((record, vec![vec![DynamicValue::None; padding]]));
+                    }
 
                     let target = if let Some(input_dir) = &args.flag_input_dir {
-                        ScraperTarget::HtmlFile(input_dir, cell)
+                        ScraperTarget::HtmlFile(
+                            input_dir,
+                            std::str::from_utf8(cell).expect("invalid utf-8"),
+                        )
                     } else {
                         ScraperTarget::HtmlCell(cell)
                     };
@@ -303,15 +370,26 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         let mut index: usize = 0;
 
         while reader.read_byte_record(&mut record)? {
-            let cell = std::str::from_utf8(&record[column_index]).expect("invalid utf-8");
+            let cell = &record[column_index];
 
-            let target = if let Some(input_dir) = &args.flag_input_dir {
-                ScraperTarget::HtmlFile(input_dir, cell)
-            } else {
-                ScraperTarget::HtmlCell(cell)
+            let output_rows = {
+                if cell.trim().is_empty() {
+                    vec![vec![DynamicValue::None; padding]]
+                } else {
+                    let target = if let Some(input_dir) = &args.flag_input_dir {
+                        ScraperTarget::HtmlFile(
+                            input_dir,
+                            std::str::from_utf8(cell).expect("invalid utf-8"),
+                        )
+                    } else {
+                        ScraperTarget::HtmlCell(cell)
+                    };
+
+                    scraper.scrape(index, &record, target)?
+                }
             };
 
-            for output_row in scraper.scrape(index, &record, target)? {
+            for output_row in output_rows {
                 let mut output_record = if let Some(keep_sel) = &keep {
                     keep_sel.select(&record).collect()
                 } else {
