c08f09895393e509c6f3ef40f5fa3a053c535a33
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2025-04-27 09:44:01 +0530

    Update functions.rs

--- src/moonblade/functions.rs
+++ src/moonblade/functions.rs
@@ -132,8 +132,8 @@ pub fn get_function(name: &str) -> Option<(Function, FunctionArguments)> {
         "log" => (
             |args| {
                 match args.len() {
-                    1 => unary_arithmetic_op(args, DynamicNumber::log),
-                    2 => binary_arithmetic_op(args, DynamicNumber::log),
+                    1 => unary_arithmetic_op(args, |arg: DynamicNumber| arg.log(None)),
+                    2 => binary_arithmetic_op(args, |arg0: DynamicNumber, arg1: DynamicNumber| arg0.log(Some(arg1))),
                 }
             },
             FunctionArguments::with_range(1..=2),
