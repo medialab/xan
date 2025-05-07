d36aee18d8ea1163e66ef1062b8aee6693ac9300
Yomguithereal <guillaumeplique@gmail.com>
2025-05-07 16:56:06 +0200

    Adding a functional test for file segmentation

---
+++ scripts/test_segmentation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -uoe pipefail
+shopt -s lastpipe
+
+cargo build --release
+
+XAN=./target/release/xan
+
+echo $($XAN count $1)
+
+for i in $(seq 1 64);
+do
+  declare -i total=0
+
+  $XAN split --chunks $i --segments . $1 | $XAN select -e 'fmt("-B {} -E {}", from, to)' | $XAN behead | \
+  while read -r segment;
+  do
+    count=$($XAN slice $segment $1 | $XAN count)
+    ((total += $count))
+  done
+
+  ((total--))
+
+  echo "--chunks=$i -> $total"
+done
