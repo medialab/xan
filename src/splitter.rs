c2229970ef3ece7f25b0d9aa67bdff77801aacdd
Yomguithereal <guillaumeplique@gmail.com>
2025-07-11 15:15:55 +0200

    Correct implementation of splitter?

--- src/splitter.rs
+++ src/splitter.rs
@@ -11,23 +11,24 @@ enum SplitRecordResult {
 
 #[derive(Debug)]
 enum RecordSplitterState {
-    InUnquoted,
-    InQuoted,
-    InQuote,
+    Unquoted,
+    Quoted,
+    Quote,
 }
 
 struct RecordSplitter {
     quote: u8,
     state: RecordSplitterState,
-    was_empty: bool,
+    record_was_read: bool,
 }
 
 impl RecordSplitter {
     fn new(quote: u8) -> Self {
         Self {
             quote,
-            state: RecordSplitterState::InUnquoted,
-            was_empty: false,
+            state: RecordSplitterState::Unquoted,
+            // Must be true at the beginning to avoid counting one record for empty input
+            record_was_read: true,
         }
     }
 
@@ -35,19 +36,28 @@ impl RecordSplitter {
         use RecordSplitterState::*;
 
         if input.is_empty() {
-            if !self.was_empty {
-                self.was_empty = true;
+            if !self.record_was_read {
+                self.record_was_read = true;
                 return (SplitRecordResult::Record, 0);
             }
 
             return (SplitRecordResult::End, 0);
         }
 
+        self.record_was_read = false;
+
         let mut pos: usize = 0;
 
         while pos < input.len() {
             match self.state {
-                InUnquoted => {
+                Unquoted => {
+                    // Skipping empty lines
+                    if input[pos] == b'\n' {
+                        pos += 1;
+                        self.record_was_read = true;
+                        continue;
+                    }
+
                     // Here we are moving to next quote or end of line
                     if let Some(offset) = memchr2(b'\n', self.quote, &input[pos..]) {
                         pos += offset;
@@ -57,31 +67,32 @@ impl RecordSplitter {
                         pos += 1;
 
                         if byte == b'\n' {
+                            self.record_was_read = true;
                             return (SplitRecordResult::Record, pos);
                         }
 
                         // Here, c is guaranteed to be a quote
-                        self.state = InQuoted;
+                        self.state = Quoted;
                     } else {
                         break;
                     }
                 }
-                InQuoted => {
+                Quoted => {
                     // Here we moving to next quote
                     if let Some(offset) = memchr(self.quote, &input[pos..]) {
                         pos += offset + 1;
-                        self.state = InQuote;
+                        self.state = Quote;
                     } else {
                         break;
                     }
                 }
-                InQuote => {
+                Quote => {
                     let byte = input[pos];
 
                     if byte == self.quote {
-                        self.state = InQuoted;
+                        self.state = Quoted;
                     } else {
-                        self.state = InUnquoted;
+                        self.state = Unquoted;
                     }
 
                     pos += 1;
@@ -93,6 +104,7 @@ impl RecordSplitter {
     }
 }
 
+// TODO: capacity
 pub fn count_records<R: Read>(reader: R, quote: u8) -> Result<u64> {
     use SplitRecordResult::*;
 
@@ -121,6 +133,9 @@ pub fn count_records<R: Read>(reader: R, quote: u8) -> Result<u64> {
 
 // TEST: empty fields, empty lines, empty input, clrf, invalid quoted parse, test stopping parser right on quote,
 // don't count empty lines (need to trim)
+// TODO: test quote lol and invalid last row?
+// TODO: emit record, trimmed, with mmap
+// TODO: BOM
 
 #[cfg(test)]
 mod tests {
@@ -130,8 +145,30 @@ mod tests {
 
     #[test]
     fn test_count() {
-        let mut reader = Cursor::new("name\njohn\nlucy");
+        // Empty
+        assert_eq!(count_records(&mut Cursor::new(""), b'"').unwrap(), 0);
 
+        // Single cells with various empty lines
+        let tests = vec![
+            "name\njohn\nlucy",
+            "name\njohn\nlucy\n",
+            "name\n\njohn\r\nlucy\n",
+            "name\n\njohn\r\nlucy\n\n",
+            "\nname\njohn\nlucy",
+        ];
+
+        for test in tests {
+            let mut reader = Cursor::new(test);
+
+            assert_eq!(count_records(&mut reader, b'"').unwrap(), 3, "{:?}", test);
+        }
+
+        // Multiple cells
+        let mut reader = Cursor::new("name,surname,age\njohn,landy,45\nlucy,rose,67");
+        assert_eq!(count_records(&mut reader, b'"').unwrap(), 3);
+
+        // Quoting
+        let mut reader = Cursor::new("name,surname,age\n\"john\",\"landy, the \"\"everlasting\"\" bastard\",45\nlucy,rose,67");
         assert_eq!(count_records(&mut reader, b'"').unwrap(), 3);
     }
 }
