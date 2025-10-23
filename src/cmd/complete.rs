51e648f0e1934cfb93336b57fcb689ee16fa1973
echevale <evan.chevalerias@gmail.com>
2025-10-23 10:53:53 +0200

    Added the flag --dates to complete cmd

--- src/cmd/complete.rs
+++ src/cmd/complete.rs
@@ -3,6 +3,7 @@ use std::io::{stdout, Write};
 use csv::StringRecord;
 
 use crate::config::{Config, Delimiter};
+use crate::dates;
 use crate::select::SelectColumns;
 use crate::util;
 use crate::CliResult;
@@ -22,6 +23,8 @@ complete options:
     -z, --zero <value>       The value to fill in the completed rows.
                              Default is an empty string.
     --check                  Check that the input is complete.
+    -D, --dates              Set to indicate your values are dates (supporting year, year-month or
+                             year-month-day).
 
 Common options:
     -h, --help               Display this message
@@ -36,13 +39,14 @@ Common options:
 struct Args {
     arg_columns: SelectColumns,
     arg_input: Option<String>,
-    flag_min: Option<i32>,
-    flag_max: Option<i32>,
+    flag_min: Option<String>,
+    flag_max: Option<String>,
     flag_output: Option<String>,
     flag_no_headers: bool,
     flag_delimiter: Option<Delimiter>,
     flag_zero: Option<String>,
     flag_check: bool,
+    flag_dates: bool,
 }
 
 pub fn run(argv: &[&str]) -> CliResult<()> {
@@ -53,91 +57,245 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         .select(args.arg_columns)
         .delimiter(args.flag_delimiter);
 
-    let mut wtr = Config::new(&args.flag_output).writer()?;
+    let mut wtr_opt = if args.flag_check {
+        None
+    } else {
+        Some(Config::new(&args.flag_output).writer()?)
+    };
 
     let mut rdr = rconf.reader()?;
     let headers = rdr.byte_headers()?.clone();
 
-    let sel = rconf.selection(&headers)?;
-    let mut index: Option<i32> = None;
-
-    if let Some(min) = args.flag_min {
-        index = Some(min);
-    }
-
+    // TODO: use a ByteRecord
     let mut record = StringRecord::new();
 
-    if args.flag_check {
+    let sel = rconf.selection(&headers)?;
+
+    if let Some(wtr) = wtr_opt.as_mut() {
+        wtr.write_record(&headers)?;
+    }
+
+    let zero = args.flag_zero.unwrap_or_else(|| "".to_string());
+
+    if args.flag_dates {
+        let mut index: Option<dates::PartialDate> = None;
+
+        if let Some(min) = &args.flag_min {
+            if let Some(max) = &args.flag_max {
+                if dates::parse_partial_date(&min).unwrap().into_inner()
+                    > dates::parse_partial_date(&max).unwrap().into_inner()
+                {
+                    Err("min cannot be greater than max")?;
+                }
+            }
+            index = Some(dates::parse_partial_date(&min).ok_or("invalid min date")?);
+        }
+
         while rdr.read_record(&mut record)? {
-            let value = sel
-                .select(&record)
-                .map(|i| i.parse::<i32>().unwrap())
-                .next();
+            let value = dates::parse_partial_date(&sel.select(&record).next().unwrap().to_string())
+                .ok_or("invalide date in file")?;
+
+            if let Some(min) = &args.flag_min {
+                // skip values below min of the range
+                // TODO: handle min/max properly for dates
+                if value.clone().into_inner() < dates::parse_partial_date(min).unwrap().into_inner()
+                {
+                    continue;
+                }
+            }
+
+            if let Some(max) = &args.flag_max {
+                // stop completing or checking if we go over max of the range
+                if value.clone().into_inner() > dates::parse_partial_date(max).unwrap().into_inner()
+                {
+                    break;
+                }
+            }
+
+            if index.clone().is_some() {
+                if let Some(wtr) = wtr_opt.as_mut() {
+                    while value.clone().into_inner() > index.clone().unwrap().into_inner() {
+                        let mut new_record = StringRecord::new();
+                        for cell in sel.indexed_mask(record.len()) {
+                            if cell.is_some() {
+                                new_record.push_field(&dates::format_partial_date(
+                                    index.clone().unwrap().as_unit(),
+                                    &index.clone().unwrap().into_inner(),
+                                ));
+                            } else {
+                                new_record.push_field(&zero);
+                            }
+                        }
+                        index = Some(
+                            dates::parse_partial_date(&dates::format_partial_date(
+                                index.clone().unwrap().as_unit(),
+                                &dates::next_partial_date(
+                                    index.clone().unwrap().as_unit(),
+                                    &index.clone().unwrap().into_inner(),
+                                ),
+                            ))
+                            .unwrap(),
+                        );
+                        wtr.write_record(&new_record)?;
+                    }
+                } else {
+                    // in case of using min or if there are repeated values
+                    if value.clone().into_inner() < index.clone().unwrap().into_inner() {
+                        continue;
+                    }
+                    if value != index.clone().unwrap() {
+                        Err(format!(
+                            "file is not complete: missing value {:?}",
+                            index.clone().unwrap()
+                        ))?;
+                    }
+                }
+            } else {
+                index = Some(value);
+            }
+            let unit = index.clone().unwrap().as_unit();
+            index = Some(
+                dates::parse_partial_date(&dates::format_partial_date(
+                    unit,
+                    &dates::next_partial_date(unit, &index.clone().unwrap().into_inner()),
+                ))
+                .unwrap(),
+            );
+            if let Some(wtr) = wtr_opt.as_mut() {
+                wtr.write_record(&record)?;
+            }
+        }
+
+        if let Some(max) = args.flag_max {
+            let max = dates::parse_partial_date(&max).ok_or("invalid max date")?;
+            if let Some(wtr) = wtr_opt.as_mut() {
+                while index.clone().is_some()
+                    && index.clone().unwrap().into_inner() <= max.clone().into_inner()
+                {
+                    let mut new_record = StringRecord::new();
+                    for cell in sel.indexed_mask(headers.len()) {
+                        if cell.is_some() {
+                            new_record.push_field(&dates::format_partial_date(
+                                index.clone().unwrap().as_unit(),
+                                &index.clone().unwrap().into_inner(),
+                            ));
+                        } else {
+                            new_record.push_field(&zero);
+                        }
+                    }
+                    let unit = index.clone().unwrap().as_unit();
+                    index = Some(
+                        dates::parse_partial_date(&dates::format_partial_date(
+                            unit,
+                            &dates::next_partial_date(unit, &index.clone().unwrap().into_inner()),
+                        ))
+                        .unwrap(),
+                    );
+                    wtr.write_record(&new_record)?;
+                }
+            } else {
+                if index.clone().unwrap().into_inner() <= max.clone().into_inner() {
+                    Err(format!(
+                        "file is not complete: missing value {:?}",
+                        index.clone().unwrap()
+                    ))?;
+                }
+            }
+        }
+    } else {
+        let mut index: Option<i64> = None;
+
+        if let Some(min) = &args.flag_min {
+            if let Some(max) = &args.flag_max {
+                if min.parse::<i64>().unwrap() > max.parse::<i64>().unwrap() {
+                    Err("min cannot be greater than max")?;
+                }
+            }
+            index = Some(min.parse::<i64>().unwrap());
+        }
+
+        while rdr.read_record(&mut record)? {
+            let value = sel.select(&record).next().unwrap().parse::<i64>().unwrap();
+
+            if let Some(min) = &args.flag_min {
+                // skip values below min of the range
+                if value < min.parse::<i64>().unwrap() {
+                    continue;
+                }
+            }
+
+            if let Some(max) = &args.flag_max {
+                // stop completing or checking if we go over max of the range
+                if value > max.parse::<i64>().unwrap() {
+                    break;
+                }
+            }
 
             if index.is_some() {
-                if value.unwrap() != index.unwrap() {
+                if let Some(wtr) = wtr_opt.as_mut() {
+                    while value > index.unwrap() {
+                        let mut new_record = StringRecord::new();
+                        for cell in sel.indexed_mask(record.len()) {
+                            if cell.is_some() {
+                                new_record.push_field(&index.unwrap().to_string());
+                            } else {
+                                new_record.push_field(&zero);
+                            }
+                        }
+                        index = Some(index.unwrap() + 1);
+                        wtr.write_record(&new_record)?;
+                    }
+                } else {
+                    // in case of using min or if there are repeated values
+                    if value < index.unwrap() {
+                        continue;
+                    }
+                    if value > index.unwrap() {
+                        Err(format!(
+                            "file is not complete: missing value {}",
+                            index.unwrap()
+                        ))?;
+                    }
+                }
+            } else {
+                index = Some(value);
+            }
+            index = Some(index.unwrap() + 1);
+            if let Some(wtr) = wtr_opt.as_mut() {
+                wtr.write_record(&record)?;
+            }
+        }
+
+        if let Some(max) = args.flag_max {
+            let max = max.parse::<i64>().unwrap();
+            if let Some(wtr) = wtr_opt.as_mut() {
+                while index.is_some() && index.unwrap() <= max {
+                    let mut new_record = StringRecord::new();
+                    for cell in sel.indexed_mask(headers.len()) {
+                        if cell.is_some() {
+                            new_record.push_field(&index.unwrap().to_string());
+                        } else {
+                            new_record.push_field(&zero);
+                        }
+                    }
+                    index = Some(index.unwrap() + 1);
+                    wtr.write_record(&new_record)?;
+                }
+            } else {
+                if index.unwrap() <= max {
                     Err(format!(
                         "file is not complete: missing value {}",
                         index.unwrap()
                     ))?;
                 }
-            } else {
-                index = Some(value.unwrap());
             }
-            index = Some(index.unwrap() + 1);
         }
+    }
 
+    if let Some(wtr) = wtr_opt.as_mut() {
+        Ok(wtr.flush()?)
+    } else {
         writeln!(&mut stdout(), "file is complete!")?;
-
-        return Ok(());
+        Ok(())
     }
-
-    let zero = args.flag_zero.unwrap_or_else(|| "".to_string());
-
-    wtr.write_record(&headers)?;
-
-    while rdr.read_record(&mut record)? {
-        let value = sel
-            .select(&record)
-            .map(|i| i.parse::<i32>().unwrap())
-            .next();
-
-        if index.is_some() {
-            while value.unwrap() > index.unwrap() {
-                let mut new_record = StringRecord::new();
-                for cell in sel.indexed_mask(record.len()) {
-                    if cell.is_some() {
-                        new_record.push_field(&index.unwrap().to_string());
-                    } else {
-                        new_record.push_field(&zero);
-                    }
-                }
-                index = Some(index.unwrap() + 1);
-                wtr.write_record(&new_record)?;
-            }
-            if index.unwrap() == value.unwrap() {
-                index = Some(index.unwrap() + 1);
-            }
-        } else {
-            index = Some(value.unwrap() + 1);
-        }
-        wtr.write_record(&record)?;
-    }
-
-    if let Some(max) = args.flag_max {
-        while index.is_some() && index.unwrap() <= max {
-            let mut new_record = StringRecord::new();
-            for cell in sel.indexed_mask(headers.len()) {
-                if cell.is_some() {
-                    new_record.push_field(&index.unwrap().to_string());
-                } else {
-                    new_record.push_field(&zero);
-                }
-            }
-            index = Some(index.unwrap() + 1);
-            wtr.write_record(&new_record)?;
-        }
-    }
-
-    Ok(wtr.flush()?)
 }
--- tests/test_complete.rs
+++ tests/test_complete.rs
@@ -10,6 +10,218 @@ fn people() -> Vec<Vec<String>> {
     ]
 }
 
+fn dates() -> Vec<Vec<String>> {
+    vec![
+        svec!["date", "event"],
+        svec!["2025-01", "event1"],
+        svec!["2025-03", "event2"],
+        svec!["2025-06", "event3"],
+    ]
+}
+
+#[test]
+#[should_panic]
+fn test_complete_check_panic() {
+    let wrk = Workdir::new("complete_check_panic");
+    wrk.create("indexes.csv", people());
+    let mut cmd = wrk.command("complete");
+    cmd.arg("id").arg("indexes.csv").arg("--check");
+    let _got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+}
+
+#[test]
+#[should_panic]
+fn test_complete_check_panic_min() {
+    let wrk = Workdir::new("complete_check_panic_min");
+    wrk.create(
+        "indexes.csv",
+        vec![
+            svec!["id", "name"],
+            svec!["0", "alice"],
+            svec!["1", "bob"],
+            svec!["2", "charlie"],
+            svec!["3", "dave"],
+        ],
+    );
+    let mut cmd = wrk.command("complete");
+    cmd.arg("id")
+        .arg("indexes.csv")
+        .arg("--check")
+        .arg("-m")
+        .arg("-1");
+    let _got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+}
+
+#[test]
+#[should_panic]
+fn test_complete_check_panic_max() {
+    let wrk = Workdir::new("complete_check_panic_max");
+    wrk.create(
+        "indexes.csv",
+        vec![
+            svec!["id", "name"],
+            svec!["0", "alice"],
+            svec!["1", "bob"],
+            svec!["2", "charlie"],
+            svec!["3", "dave"],
+        ],
+    );
+    let mut cmd = wrk.command("complete");
+    cmd.arg("id")
+        .arg("indexes.csv")
+        .arg("--check")
+        .arg("-M")
+        .arg("5");
+    let _got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+}
+
+#[test]
+fn test_complete_check() {
+    let wrk = Workdir::new("complete_check");
+    wrk.create(
+        "indexes_complete.csv",
+        vec![
+            svec!["id", "name"],
+            svec!["0", "alice"],
+            svec!["1", "bob"],
+            svec!["2", "charlie"],
+            svec!["3", "dave"],
+        ],
+    );
+    let mut cmd = wrk.command("complete");
+    cmd.arg("id").arg("indexes_complete.csv").arg("--check");
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![svec!["file is complete!"]];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn test_complete_check_min_max() {
+    let wrk = Workdir::new("complete_check_min_max");
+    wrk.create("indexes_complete.csv", people());
+    let mut cmd = wrk.command("complete");
+    cmd.arg("id")
+        .arg("indexes_complete.csv")
+        .arg("--check")
+        .arg("-m")
+        .arg("2")
+        .arg("-M")
+        .arg("3");
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![svec!["file is complete!"]];
+    assert_eq!(got, expected);
+}
+
+#[test]
+#[should_panic]
+fn test_complete_check_dates_panic() {
+    let wrk = Workdir::new("complete_check_dates_panic");
+    wrk.create("dates_incomplete.csv", dates());
+    let mut cmd = wrk.command("complete");
+    cmd.arg("date")
+        .arg("dates_incomplete.csv")
+        .arg("--dates")
+        .arg("--check");
+    let _got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+}
+
+#[test]
+#[should_panic]
+fn test_complete_check_dates_panic_min() {
+    let wrk = Workdir::new("complete_check_dates_panic_min");
+    wrk.create(
+        "dates_incomplete.csv",
+        vec![
+            svec!["date", "event"],
+            svec!["2025-02", "event1"],
+            svec!["2025-03", "event2"],
+            svec!["2025-04", "event3"],
+        ],
+    );
+    let mut cmd = wrk.command("complete");
+    cmd.arg("date")
+        .arg("dates_incomplete.csv")
+        .arg("--dates")
+        .arg("--check")
+        .arg("-m")
+        .arg("2025-01");
+    let _got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+}
+
+#[test]
+#[should_panic]
+fn test_complete_check_dates_panic_max() {
+    let wrk = Workdir::new("complete_check_dates_panic_max");
+    wrk.create(
+        "dates_incomplete.csv",
+        vec![
+            svec!["date", "event"],
+            svec!["2025-02", "event1"],
+            svec!["2025-03", "event2"],
+            svec!["2025-04", "event3"],
+        ],
+    );
+    let mut cmd = wrk.command("complete");
+    cmd.arg("date")
+        .arg("dates_incomplete.csv")
+        .arg("--dates")
+        .arg("--check")
+        .arg("-M")
+        .arg("2025-06");
+    let _got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+}
+
+#[test]
+fn test_complete_check_dates() {
+    let wrk = Workdir::new("complete_check_dates");
+    wrk.create(
+        "dates_complete.csv",
+        vec![
+            svec!["date", "event"],
+            svec!["2025-01", "event1"],
+            svec!["2025-02", "event2"],
+            svec!["2025-03", "event3"],
+        ],
+    );
+    let mut cmd = wrk.command("complete");
+    cmd.arg("date")
+        .arg("dates_complete.csv")
+        .arg("--dates")
+        .arg("--check");
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![svec!["file is complete!"]];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn test_complete_check_dates_min_max() {
+    let wrk = Workdir::new("complete_check_dates_min_max");
+    wrk.create(
+        "dates_complete.csv",
+        vec![
+            svec!["date", "event"],
+            svec!["2025-02", "event1"],
+            svec!["2025-03", "event2"],
+            svec!["2025-04", "event3"],
+            svec!["2025-05", "event4"],
+            svec!["2025-06", "event5"],
+            svec!["2025-07", "event6"],
+        ],
+    );
+    let mut cmd = wrk.command("complete");
+    cmd.arg("date")
+        .arg("dates_complete.csv")
+        .arg("--dates")
+        .arg("--check")
+        .arg("-m")
+        .arg("2025-03")
+        .arg("-M")
+        .arg("2025-05");
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![svec!["file is complete!"]];
+    assert_eq!(got, expected);
+}
+
 #[test]
 fn test_complete_basic() {
     let wrk = Workdir::new("complete_basic");
@@ -33,17 +245,17 @@ fn test_complete_basic() {
 
 #[test]
 fn test_complete_with_min_max() {
-    let wrk = Workdir::new("complete_with_min_max");
-    wrk.create("indexes.csv", people());
-    let mut cmd = wrk.command("complete");
-    cmd.arg("id")
+    let wrk1 = Workdir::new("complete_with_min_max");
+    wrk1.create("indexes.csv", people());
+    let mut cmd1 = wrk1.command("complete");
+    cmd1.arg("id")
         .arg("indexes.csv")
         .arg("-m")
         .arg("-2")
         .arg("-M")
         .arg("8");
-    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-    let expected = vec![
+    let got1: Vec<Vec<String>> = wrk1.read_stdout(&mut cmd1);
+    let expected1 = vec![
         svec!["id", "name"],
         svec!["-2", ""],
         svec!["-1", ""],
@@ -57,7 +269,27 @@ fn test_complete_with_min_max() {
         svec!["7", "dave"],
         svec!["8", ""],
     ];
-    assert_eq!(got, expected);
+    assert_eq!(got1, expected1);
+
+    let wrk2 = Workdir::new("complete_with_min_max_dropping");
+    wrk2.create("indexes.csv", people());
+    let mut cmd2 = wrk2.command("complete");
+    cmd2.arg("id")
+        .arg("indexes.csv")
+        .arg("-m")
+        .arg("1")
+        .arg("-M")
+        .arg("5");
+    let got2: Vec<Vec<String>> = wrk2.read_stdout(&mut cmd2);
+    let expected2 = vec![
+        svec!["id", "name"],
+        svec!["1", ""],
+        svec!["2", "bob"],
+        svec!["3", "charlie"],
+        svec!["4", ""],
+        svec!["5", ""],
+    ];
+    assert_eq!(got2, expected2);
 }
 
 #[test]
@@ -82,31 +314,69 @@ fn test_complete_with_zero_value() {
 }
 
 #[test]
-#[should_panic]
-fn test_complete_check_complete_panic() {
-    let wrk = Workdir::new("complete_check_complete_panic");
-    wrk.create("indexes.csv", people());
+fn test_complete_dates() {
+    let wrk = Workdir::new("complete_dates");
+    wrk.create("dates_incomplete.csv", dates());
     let mut cmd = wrk.command("complete");
-    cmd.arg("id").arg("indexes.csv").arg("--check");
-    let _got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    cmd.arg("date").arg("dates_incomplete.csv").arg("--dates");
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["date", "event"],
+        svec!["2025-01", "event1"],
+        svec!["2025-02", ""],
+        svec!["2025-03", "event2"],
+        svec!["2025-04", ""],
+        svec!["2025-05", ""],
+        svec!["2025-06", "event3"],
+    ];
+    assert_eq!(got, expected);
 }
 
 #[test]
-fn test_complete_check_complete() {
-    let wrk = Workdir::new("complete_check_complete");
-    wrk.create(
-        "indexes_complete.csv",
-        vec![
-            svec!["id", "name"],
-            svec!["0", "alice"],
-            svec!["1", "bob"],
-            svec!["2", "charlie"],
-            svec!["3", "dave"],
-        ],
-    );
-    let mut cmd = wrk.command("complete");
-    cmd.arg("id").arg("indexes_complete.csv").arg("--check");
-    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-    let expected = vec![svec!["file is complete!"]];
-    assert_eq!(got, expected);
+fn test_complete_dates_with_min_max() {
+    let wrk1 = Workdir::new("complete_dates_with_min_max");
+    wrk1.create("dates_incomplete.csv", dates());
+    let mut cmd1 = wrk1.command("complete");
+    cmd1.arg("date")
+        .arg("dates_incomplete.csv")
+        .arg("--dates")
+        .arg("-m")
+        .arg("2024-11")
+        .arg("-M")
+        .arg("2025-08");
+    let got1: Vec<Vec<String>> = wrk1.read_stdout(&mut cmd1);
+    let expected1 = vec![
+        svec!["date", "event"],
+        svec!["2024-11", ""],
+        svec!["2024-12", ""],
+        svec!["2025-01", "event1"],
+        svec!["2025-02", ""],
+        svec!["2025-03", "event2"],
+        svec!["2025-04", ""],
+        svec!["2025-05", ""],
+        svec!["2025-06", "event3"],
+        svec!["2025-07", ""],
+        svec!["2025-08", ""],
+    ];
+    assert_eq!(got1, expected1);
+
+    let wrk2 = Workdir::new("complete_dates_with_min_max_dropping");
+    wrk2.create("dates_incomplete.csv", dates());
+    let mut cmd2 = wrk2.command("complete");
+    cmd2.arg("date")
+        .arg("dates_incomplete.csv")
+        .arg("--dates")
+        .arg("-m")
+        .arg("2025-02")
+        .arg("-M")
+        .arg("2025-05");
+    let got2: Vec<Vec<String>> = wrk2.read_stdout(&mut cmd2);
+    let expected2 = vec![
+        svec!["date", "event"],
+        svec!["2025-02", ""],
+        svec!["2025-03", "event2"],
+        svec!["2025-04", ""],
+        svec!["2025-05", ""],
+    ];
+    assert_eq!(got2, expected2);
 }
