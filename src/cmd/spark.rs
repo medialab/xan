3fc7590112eb9676e9278199cbbf55f1265e13fd
Yomguithereal <guillaumeplique@gmail.com>
2026-05-11 21:24:21 +0200

    xan spark --along-rows Related to #448

--- src/cmd/spark.rs
+++ src/cmd/spark.rs
@@ -4,6 +4,7 @@ use std::num::NonZeroUsize;
 
 use colored::Colorize;
 use simd_csv::ByteRecord;
+use unicode_width::UnicodeWidthStr;
 
 use crate::config::{Config, Delimiter};
 use crate::scales::{ExtentBuilder, Scale, ScaleType};
@@ -62,21 +63,46 @@ pub struct SparklineRenderer {
 impl SparklineRenderer {
     #[inline(always)]
     pub fn render(&mut self, scale: &Scale, bins: &[f64]) {
-        self.render_with_color_overrides(scale, bins, None);
+        self.render_impl(None, scale, bins, None);
     }
 
+    #[inline(always)]
     pub fn render_with_color_overrides(
         &mut self,
         scale: &Scale,
         bins: &[f64],
         color_overrides_opt: Option<&[Option<ColorOrStyles>]>,
     ) {
+        self.render_impl(None, scale, bins, color_overrides_opt);
+    }
+
+    pub fn render_impl(
+        &mut self,
+        name_opt: Option<&str>,
+        scale: &Scale,
+        bins: &[f64],
+        color_overrides_opt: Option<&[Option<ColorOrStyles>]>,
+    ) {
+        let name_width = name_opt.map(|name| name.width()).unwrap_or(0);
+
         let height = self.options.height;
         let width = self.options.width;
 
         self.draw_buffer.clear();
 
         for h in (0..height).rev() {
+            match (h, name_opt) {
+                (0, Some(name)) => {
+                    self.draw_buffer.push_str(name);
+                }
+                (_, Some(_)) => {
+                    for _ in 0..name_width {
+                        self.draw_buffer.push(' ');
+                    }
+                }
+                _ => (),
+            };
+
             let len = SPARKLINE_CHARS.len();
 
             for (i, y) in bins.iter().copied().enumerate() {
@@ -187,6 +213,14 @@ struct Series {
 
 impl Series {
     #[inline]
+    fn new() -> Self {
+        Self {
+            extent_builder: ExtentBuilder::new(),
+            numbers: Vec::new(),
+        }
+    }
+
+    #[inline]
     fn with_capacity(capacity: usize) -> Self {
         Self {
             extent_builder: ExtentBuilder::new(),
@@ -249,6 +283,7 @@ Usage:
     xan spark --help
 
 spark options:
+    --along-rows
     -W, --width <n>   Number of characters a sparkline bar is allowed to take as
                       its width.
                       [default: 1]
@@ -280,12 +315,19 @@ struct Args {
     cmd_debate: bool,
     arg_input: Option<String>,
     arg_columns: SelectedColumns,
+    flag_along_rows: bool,
     flag_width: NonZeroUsize,
     flag_height: NonZeroUsize,
     flag_cols: Option<String>,
     flag_color: ColorMode,
     flag_no_headers: bool,
     flag_delimiter: Option<Delimiter>,
+}
+
+impl Args {
+    fn series_have_name(&self) -> bool {
+        !self.flag_along_rows
+    }
 }
 
 pub fn run(argv: &[&str]) -> CliResult<()> {
@@ -310,7 +352,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
 
     let rconf = Config::new(&args.arg_input)
         .delimiter(args.flag_delimiter)
-        .select(args.arg_columns)
+        .select(args.arg_columns.clone())
         .no_headers(args.flag_no_headers);
 
     let mut reader = rconf.simd_reader()?;
@@ -319,22 +361,54 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
 
     let mut record = ByteRecord::new();
 
-    let mut pool: Vec<Series> = Vec::new();
+    let mut pool: Vec<(Option<String>, Series)> = Vec::new();
+
+    if !args.flag_along_rows {
+        pool.reserve_exact(sel.len());
+
+        for name in sel.select(&headers) {
+            pool.push((
+                Some(String::from_utf8_lossy(name).into_owned()),
+                Series::new(),
+            ));
+        }
+    }
 
     // Aggregating data
     while reader.read_byte_record(&mut record)? {
-        let mut series = Series::with_capacity(sel.len());
+        if args.flag_along_rows {
+            let mut series = Series::with_capacity(sel.len());
 
-        for cell in sel.select(&record) {
-            series.try_push(cell)?;
+            for cell in sel.select(&record) {
+                series.try_push(cell)?;
+            }
+
+            pool.push((None, series));
+        } else {
+            for (i, cell) in sel.select(&record).enumerate() {
+                pool[i].1.try_push(cell)?;
+            }
         }
-
-        pool.push(series);
     }
 
-    let cols_for_sparkline = cols;
+    let mut cols_for_sparkline = cols;
     let sparkline_width = args.flag_width.get();
     let sparkline_height = args.flag_height.get();
+
+    let mut cols_for_series_name: usize = 0;
+
+    if args.series_have_name() {
+        let max_name_width = pool
+            .iter()
+            .map(|(name_opt, _)| name_opt.as_ref().map(|name| name.width()).unwrap_or(0))
+            .max()
+            .unwrap()
+            + 1;
+
+        cols_for_series_name = max_name_width.min((cols as f64 * 0.3).floor() as usize);
+        cols_for_sparkline -= cols_for_series_name;
+    }
+
     let max_bins = cols_for_sparkline / sparkline_width;
 
     let mut sparkline_renderer_options = SparklineRendererOptions::new();
@@ -344,13 +418,24 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     let mut sparkline_renderer = sparkline_renderer_options.build();
 
     // Rendering
-    for mut series in pool.into_iter() {
+    for (name_opt, mut series) in pool.into_iter() {
         if series.len() > max_bins {
             series.discretize(max_bins);
         }
 
+        let name_opt = name_opt.map(|name| {
+            format!(
+                "{:<width$} ",
+                util::unicode_aware_ellipsis(
+                    &util::sanitize_text_for_multi_line_printing(&name),
+                    cols_for_series_name.saturating_sub(1),
+                ),
+                width = cols_for_series_name.saturating_sub(1)
+            )
+        });
+
         let scale = series.to_scale(ScaleType::Linear).unwrap();
-        sparkline_renderer.render(&scale, &series.numbers);
+        sparkline_renderer.render_impl(name_opt.as_deref(), &scale, &series.numbers, None);
 
         writeln!(&mut out, "{}", sparkline_renderer)?;
     }
